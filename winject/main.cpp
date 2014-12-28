34397955920d2f9ebba11035212806fbd57db14d
osvein <osvein@users.noreply.github.com>
2014-12-29 00:47:36 +0100

    Argument parsing stub

--- winject/main.cpp
+++ winject/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+
 #include <Windows.h>
 #include <TlHelp32.h>
 
 int main(int argc, char* argv[])
 {
+        if (argc != 3)
+        {
+                return 1;
+        }
+        char* library = argv[1];
+        char* process = argv[2];
+
 	return 0;
 }
