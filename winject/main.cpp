2f7340cfa2445fb2b947429e7b7d880cb2190438
osvein <osvein@users.noreply.github.com>
2014-12-29 23:06:37 +0100

    Implemented injection

--- winject/main.cpp
+++ winject/main.cpp
@@ -3,14 +3,54 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
+typedef HINSTANCE(*fpLoadLibrary)(char*);
+
 int main(int argc, char* argv[])
 {
-        if (argc != 3)
-        {
-                return 1;
-        }
-        char* library = argv[1];
-        char* process = argv[2];
+	if (argc != 3)
+	{
+		return 1;
+	}
+	char* library = argv[1];
+	char* process = argv[2];
+
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(PROCESSENTRY32);
+
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+	DWORD pid = NULL;
+
+	if (!Process32First(snapshot, &entry))
+	{
+		return 1;
+	}
+
+	do
+	{
+		if (strcmp(entry.szExeFile, process))
+		{
+			pid = entry.th32ProcessID;
+			break;
+		}
+	} while (Process32Next(snapshot, &entry));
+
+	CloseHandle(snapshot);
+
+	HANDLE hProcess;
+
+	HINSTANCE hDll = LoadLibraryA("KERNEL32");
+
+	fpLoadLibrary LoadLibraryAddr = (fpLoadLibrary)GetProcAddress(hDll, "LoadLibraryA");
+
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+
+	LPVOID paramAddr = VirtualAllocEx(hProcess, 0, strlen(library) + 1, MEM_COMMIT, PAGE_READWRITE);
+	bool memoryWritten = WriteProcessMemory(hProcess, paramAddr, library, strlen(library) + 1, NULL);
+
+	CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryAddr, paramAddr, 0, 0);
+
+	CloseHandle(hProcess);
 
 	return 0;
 }
